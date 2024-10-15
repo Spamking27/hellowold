586a321698d712d4559d46ff492d13d1be1c0968
Constapoodle <costathebest@yahoo.lol>
2024-10-14 22:44:50 -0400

    praying rn

---
+++ badoom.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+<p>
+
+Have you prayed today: <?= htmlspecialchars($_POST['Prayers']) ?></p>
+
+<p> Have you sinned today: <?= htmlspecialchars($_POST['sins'])?></p>
+<p> Have you always left room for CHEESE: <?= int($_POST['feta']) ?> </p>
+</body>
+</html>
+
