8ee2ce4db314adb1f3226656e24deb367b63b386
Constapoodle <costathebest@yahoo.lol>
2024-10-14 22:43:41 -0400

    should work

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
-<?php phpinfo(); ?>
-<p><?= var_dump($_SERVER) ?>
+<!DOCTYPE html>
+<html>
+<body>
+<p>
+<?= $_SERVER['HTTP_USER_AGENT']  ?>
 
-<?= echo $_SERVER['HTTP_USER_AGENT']          ?>
+<form action="/hellowold/badoom.php" method="post">
+	<label for="Prayers">Have your prayed today:</label><br>
+	<input type="text" id="Prayers" name="Prayers" value="cheese"><br>
+
+	
+	<input type="submit" value="confess">
 
 
+</form>
 
+GET:<?=var_dump($_GET) ?> </p>
+POST: <?= var_dump($_POST) ?> </p>
 
 </p>
-
-
+</body>
+</html>
 
