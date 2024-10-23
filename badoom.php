920d1f0640979cc8aea8b33e5b367d4f44b4e332
Constapoodle <costathebest@yahoo.lol>
2024-10-23 13:54:26 -0400

    i did it :)

--- badoom.php
+++ badoom.php
@@ -1,15 +1,52 @@
 <!DOCTYPE html>
 <html>
-<body>
 
+<head>
+
+		<?php
+			
+			$id = htmlspecialchars($_GET["id"]);
+			$SIN = htmlspecialchars($_GET["SIN"]);
+			$server = "localhost";
+			$username = "php";
+			$password ="hellonearth";
+			$database = "for_site";
+
+			$conn = mysqli_connect($server,$username,$password,$database);
+			
+			if(!$conn){
+				die("connection failed: {myslqi_connect_error()}");
+				}
+
+			$sql ="select * from info where id='{$id}';";
+			$result= mysqli_query($conn,$sql);
+
+			$sqlv2= "insert into info (SIN) values ('$SIN');";
+			$result = mysqli_query($conn,$sqlv2);
+
+
+		?>
+
+</head>
+<body>
+		<?php
+				
+			foreach($result as $row)
+			{
+				echo "{$row['SIN']}, {$row['credit_card']}, {$row['ehtnicity']}. \n";
+
+			}
+			
+
+		?>
 
 
 <p>
-
+Hello, 
 Have you prayed today: <?= htmlspecialchars($_POST['Prayers']) ?></p>
 
 <p> Have you sinned today: <?= htmlspecialchars($_POST['sins'])?></p>
-<p> Have you always left room for CHEESE: <?= int($_POST['feta']) ?> </p>
+<p> Have you always left room for CHEESE in bed and or in other scenarios/places: <?= ($_POST['feta'])?> </p>
 </body>
 </html>
 
